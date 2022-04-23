1b80a1b530db842ef28b16b28608cb5cf7e10a58
thijsBoet <m.boet@chello.nl>
2022-04-23 20:44:28 +0200

    created time travel function

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Board from './Board';
 import { calculateWinner } from '../helpers';
 
@@ -14,52 +14,62 @@ const heading = {
 	fontSize: '36px',
 	fontWeight: 'bolder',
 	borderRadius: '50px',
+	border: '4px solid #6B5B95',
 };
 
 const subTitle = {
 	width: '200px',
-	margin: '20px auto',
+	margin: '5px auto',
 	fontFamily: 'Helvetica, Arial, sans-serif',
 	color: '#34568B',
-	fontSize: '28px',
+	fontSize: '16px',
 	fontWeight: 'bolder',
 };
 
 const Game = () => {
-	const [board, setBoard] = useState(Array(9).fill(null));
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [stepNumber, setStepNumber] = useState(0);
 	const [xIsNext, setXIsNext] = useState(true);
-	const winner = calculateWinner(board);
+	const winner = calculateWinner(history[stepNumber]);
 
 	const handleClick = index => {
-		const boardCopy = [...board];
-		if (winner || boardCopy[index]) return;
-		boardCopy[index] = xIsNext ? 'X' : 'O';
-		setBoard(boardCopy);
+		const timeInHistory = history.slice(0, stepNumber + 1);
+		const current = timeInHistory[stepNumber];
+		const squares = [...current];
+		if (winner || squares[index]) return;
+		squares[index] = xIsNext ? 'X' : 'O';
+		setHistory([...timeInHistory, squares]);
+		setStepNumber(timeInHistory.length);
 		setXIsNext(!xIsNext);
-		console.log(boardCopy, xIsNext);
-	};
-	
-	const renderMoves = () => (
-		<button style={subTitle} onClick={() => setBoard(Array(9).fill(null))}>
-			Play Again
-		</button>
-	);
-
-	const jumpTo = () => {
-
 	};
 
+	const renderMoves = () =>
+		history.map((_step, move) => {
+			const destination = move ? `Go to move #${move}` : 'Go to game start';
+			return (
+				<li key={move} style={{ listStyleType: 'none' }}>
+					<button style={subTitle} onClick={() => jumpTo(move)}>
+						{destination}
+					</button>
+				</li>
+			);
+		});
+
+	const jumpTo = step => {
+		setStepNumber(step);
+		setXIsNext(step % 2 === 0);
+	};
 
 	return (
 		<>
 			<h1 style={heading}>Tic Tac Toe</h1>
-			<Board squares={board} onClick={handleClick} />
+			<Board squares={history[stepNumber]} onClick={handleClick} />
 			<div style={subTitle}>
 				<p>
 					{winner
 						? 'Winner: ' + winner
 						: 'Next Player: ' + (xIsNext ? 'X' : 'O')}
-					{winner && renderMoves() }
+					{renderMoves()}
 				</p>
 			</div>
 		</>
